228cca814e7dcd4ec7ad5e70a5c44677b3069ec7
Matias Rolon <matirolon96@gmail.com>
2021-04-06 10:44:09 -0300

    Add React Helmet dependency for SEO

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import Products from  '../components/Products.jsx';
 
 const Home = () => {
     return (
+    <>
+        <Helmet>
+            <title> Platzi Conf Merch - Productos </title>  
+        </Helmet>
         <Products />
+    </>
     )
 }
 
